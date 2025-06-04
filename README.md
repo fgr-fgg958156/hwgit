f7697b1a8e196d6b0ea3c4dbfeed04867fc7f8c0
fgr-fgg958156 <fgg958156@gmail.com>
2025-06-04 13:06:35 +0300

    Completed hw-15

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-# Script about "Run second: Average value"
+# Script about "Run third: User portrait"
 
-## I created a script that determines the arithmetic mean of three numbers with the following conditions:
+## Part #1
 
-* Receives three numbers from the user via three prompts
-* Displays the arithmetic mean of these numbers via alert
+1. Created a script that should do the following:
+
+    * asked the user for their year of birth;
+    * asked them in which city they live;
+    * asked them for their favorite sport.
+
+2. When you click OK, we show a window that displays the following information:
+   * their age;
+   * if the user specifies Kyiv, Washington, or London, 
+     then show them the message "You live in the capital..." 
+     and in place of the dots we substitute the country whose capital is the city. 
+     Otherwise, we show them "you live in the city...", where the dots are the entered city.
+   
+## Part #2
+
+   1. We choose three sports and three champions in these sports. 
+   2. Accordingly, if the user indicates one of these sports, 
+      we show him the message Cool! Do you want to become …? 
+      and substitute the first and last name of the champion in place of the dots.
+
+   3. All this is displayed in one window (alert).
+
+   4. If in some case he does not want to enter information and clicks Cancel, 
+      show him the message – “It’s a pity that you did not want to enter your …” 
+      and indicate that he did not want to enter – date of birth, city or sport.
--- js/script.js
+++ js/script.js
@@ -1,6 +1,56 @@
-const userNum1 = +prompt("What is the user first number?");
-const userNum2 = +prompt("What is the user second number?");
-const userNum3 = +prompt("What is the user third number?");
+let userLocation = "невідомо";
+let userAge = "невідомо";
+let userWannaBe = "невідомо";
 
-const averageNum = (userNum1 + userNum2 + userNum3)/3;
-alert(`average number is ${averageNum}`);
+const userBirth = +prompt("Який ваш рік народження?");
+
+if(userBirth){
+    userAge = 2025 - userBirth;
+}
+else{
+    alert("Шкода, що Ви не захотіли ввести свій рік народження");
+}
+
+const userCity = prompt("Яка назва вашого міста (писати з великої літери)?");
+
+if(userCity === null){
+    alert("Шкода, що Ви не захотіли ввести нащву свого міста");
+}
+else {
+    switch (userCity){
+        case "Київ":
+            userLocation = "столиця України";
+            break;
+        case "Лондон":
+            userLocation = "столиця Об'єднаного Королівства";
+            break;
+        case "Вашингтон":
+            userLocation = "столиця Сполучених Штатів Америки";
+            break;
+        default:
+            userLocation = `місто ${userCity}`;
+    }
+}
+
+const userFavSport = prompt("Яка назва вашого улюбленого спорту (писати з великої літери)?");
+
+if(userFavSport === null){
+    alert("Шкода, що Ви не захотіли ввести назву свого улюбленого спорту")
+}
+else{
+    switch (userFavSport){
+        case "Футбол":
+            userWannaBe = "як Кріштіано Роналду";
+            break;
+        case "Баскетбол":
+            userWannaBe = "як Леброн Джеймс";
+            break;
+        case "Бокс":
+            userWannaBe = "як Олександр Усик";
+            break;
+        default:
+            userWannaBe = `унікальним`;
+    }
+}
+
+alert(`Ваш вік: ${userAge} \nВаша локація: ${userLocation} \nКруто! Ви хочете стати ${userWannaBe}`);
