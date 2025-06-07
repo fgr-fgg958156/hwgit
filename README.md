c5542cd869ced8cd24ff086df951f009af8b3989
fgr-fgg958156 <fgg958156@gmail.com>
2025-06-07 11:49:08 +0300

    Completed hw-16

--- README.md
+++ README.md
@@ -1,29 +1,16 @@
-# Script about "Run third: User portrait"
+# Script about "Writing loops"
 
-## Part #1
+## I wrote loops that can:
 
-1. Created a script that should do the following:
-
-    * asked the user for their year of birth;
-    * asked them in which city they live;
-    * asked them for their favorite sport.
-
-2. When you click OK, we show a window that displays the following information:
-   * their age;
-   * if the user specifies Kyiv, Washington, or London, 
-     then show them the message "You live in the capital..." 
-     and in place of the dots we substitute the country whose capital is the city. 
-     Otherwise, we show them "you live in the city...", where the dots are the entered city.
-   
-## Part #2
-
-   1. We choose three sports and three champions in these sports. 
-   2. Accordingly, if the user indicates one of these sports, 
-      we show him the message Cool! Do you want to become …? 
-      and substitute the first and last name of the champion in place of the dots.
-
-   3. All this is displayed in one window (alert).
-
-   4. If in some case he does not want to enter information and clicks Cancel, 
-      show him the message – “It’s a pity that you did not want to enter your …” 
-      and indicate that he did not want to enter – date of birth, city or sport.
+1. Print the numbers from 10 to 20 on a single line separated by a comma.
+2. Print the squares of numbers from 10 to 20.
+3. Print the multiplication table for 7.
+4. Find the sum of all integers from 1 to 15.
+5. Find the product of all integers from 15 to 35.
+6. Find the arithmetic mean of all integers from 1 to 500.
+7. Print the sum of only even numbers in the range from 30 to 80.
+8. Print all numbers in the range from 100 to 200 that are multiples of 3.
+9. Given a natural number. Find and print all its divisors on the page.
+10. Determine the number of its even divisors.
+11. Find the sum of its even divisors.
+12. Print the complete multiplication table from 1 to 10.
--- js/script.js
+++ js/script.js
@@ -1,56 +1,113 @@
-let userLocation = "невідомо";
-let userAge = "невідомо";
-let userWannaBe = "невідомо";
+//task1
+let lineOfNums = '';
 
-const userBirth = +prompt("Який ваш рік народження?");
-
-if(userBirth){
-    userAge = 2025 - userBirth;
+for (let i = 10; i <= 20; i++) {
+    lineOfNums += i + (i < 20 ? ', ' : '');
 }
-else{
-    alert("Шкода, що Ви не захотіли ввести свій рік народження");
+console.log(lineOfNums);
+
+//task2
+for (let i = 10; i < 20; i++) {
+    console.log(i ** 2);
 }
 
-const userCity = prompt("Яка назва вашого міста (писати з великої літери)?");
+//task3
+let multiplicationTableFor7 = [];
 
-if(userCity === null){
-    alert("Шкода, що Ви не захотіли ввести нащву свого міста");
+for (let i = 0; i <= 12; i++) {
+    multiplicationTableFor7.push(`${i} * 7 = ${i * 7}`);
 }
-else {
-    switch (userCity){
-        case "Київ":
-            userLocation = "столиця України";
-            break;
-        case "Лондон":
-            userLocation = "столиця Об'єднаного Королівства";
-            break;
-        case "Вашингтон":
-            userLocation = "столиця Сполучених Штатів Америки";
-            break;
-        default:
-            userLocation = `місто ${userCity}`;
+
+console.table(multiplicationTableFor7);
+
+//task4
+let integers = 0;
+
+for (let i = 1; i < 15; i++) {
+    integers += i;
+}
+
+console.log(`sum of integers from 1 to 15 = ${integers}`);
+
+//task5
+let product = 1n;
+
+for (let i = 15n; i < 35n; i++) {
+    product *= i;
+}
+
+console.log(`product from 15 to 35 = ${product}`);
+
+//task6
+let sumOfIntegers = 0;
+let lengthOfIntegers = 0;
+
+for (let i = 1; i <= 500; i++) {
+    sumOfIntegers += i;
+    lengthOfIntegers++;
+}
+
+let averageOfIntegers = sumOfIntegers/lengthOfIntegers;
+console.log(`average of integers from 1 to 500 = ${averageOfIntegers}`);
+
+//task7
+let sumOfEvenNum= 0;
+
+for (let i = 30; i <= 80; i++) {
+    sumOfEvenNum += i % 2 === 0 ? i : 0;
+}
+
+console.log(`sum of even numbers from 30 to 80 = ${sumOfEvenNum}`);
+
+//task8
+let allNumsMultipleOf3 = [];
+
+for (let i = 100; i <= 200; i++) {
+    if(i % 3 === 0) {
+        allNumsMultipleOf3.push(i);
     }
 }
 
-const userFavSport = prompt("Яка назва вашого улюбленого спорту (писати з великої літери)?");
+console.table(allNumsMultipleOf3);
 
-if(userFavSport === null){
-    alert("Шкода, що Ви не захотіли ввести назву свого улюбленого спорту")
-}
-else{
-    switch (userFavSport){
-        case "Футбол":
-            userWannaBe = "як Кріштіано Роналду";
-            break;
-        case "Баскетбол":
-            userWannaBe = "як Леброн Джеймс";
-            break;
-        case "Бокс":
-            userWannaBe = "як Олександр Усик";
-            break;
-        default:
-            userWannaBe = `унікальним`;
+//task9
+const userNum = +prompt('write NATURAL number');
+let allFactorsOfUserNum = [];
+
+for(let i = 1; i <= userNum; i++) {
+    if(userNum % i === 0) {
+        allFactorsOfUserNum.push(i);
     }
 }
 
-alert(`Ваш вік: ${userAge} \nВаша локація: ${userLocation} \nКруто! Ви хочете стати ${userWannaBe}`);
+console.table(allFactorsOfUserNum);
+
+//task10
+let allEvenFactorsOfUserNum = [];
+
+for (let i = 0; i < allFactorsOfUserNum.length; i++) {
+    if(allFactorsOfUserNum[i] % 2 === 0) {
+        allEvenFactorsOfUserNum.push(allFactorsOfUserNum[i]);
+    }
+}
+
+console.log(`all even factors of USERNUMBER = ${allEvenFactorsOfUserNum.length}`);
+
+//task11
+let sumOfAllEvenFactorsOfUserNum = 0;
+
+for (let i = 0; i < allEvenFactorsOfUserNum.length; i++) {
+    sumOfAllEvenFactorsOfUserNum += allEvenFactorsOfUserNum[i];
+}
+
+console.log(`sum of all even factors of USERNUMBER = ${sumOfAllEvenFactorsOfUserNum}`);
+
+//task12
+let fullMultiplicationTable = [];
+
+for (let i = 1; i <= 10; i++) {
+    for (let c = 0; c <= 12; c++) {
+        fullMultiplicationTable.push(`${i} * ${c} = ${i*c}`);
+    }
+}
+console.table(fullMultiplicationTable);
