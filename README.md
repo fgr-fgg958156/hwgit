af05f6e3f251e27d3af0183b4fed9fb3dc47469d
fgr-fgg958156 <fgg958156@gmail.com>
2025-06-23 12:14:28 +0300

    Completed hw-25

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# script about "Write several functions"
+# script about "Write functions"
 
-This task consists of four small tasks, for the implementation of each of them you can get 25 points:
-
-1. Given an array with elements of different types. Create a function that calculates the arithmetic mean of only the numeric elements of this array.
-2. Write a function doMath(x, znak, y), which receives 3 arguments: the numbers x and y, the string znak. The variable znak can contain: +, -, *, /, %, ^ (degree ). Display the result of the mathematical operation specified in the variable znak. Both numbers and the sign come from the user.
-3. Write a function to fill a two-dimensional array with user data. The length of the main array and internal arrays is specified by the user. The value of all elements of all arrays is specified by the user.
-4. Create a function that removes all characters from a string that we passed in the second argument. 'func(" hello world", ['l', 'd'])' will return "heo wor". The output string and characters to remove are specified by the user.
+1.Your function should work similarly to the array.shift method
+2.Your next function should work similarly to the array.reverse method
+Array methods are not allowed in the DZ
--- js/script.js
+++ js/script.js
@@ -1,100 +1,46 @@
 "use strict";
 
-//task 1
-
-const averageNumber = (array) => {
-    let sumOfNumbers = 0;
-    let numOfNumbers = 0;
-    for (let i = 0; i < array.length; i++) {
-        if(typeof array[i] === 'number') {
-            sumOfNumbers += array[i];
-            numOfNumbers++;
-        }
-    }
-
-    return numOfNumbers !== 0 ? `average number is ${sumOfNumbers/numOfNumbers}` : "this array don't have any numbers";
-}
-
-const arrayForFirstFunction = ['cat', 2, 8, 'Ukraine', true];
-console.log(averageNumber(arrayForFirstFunction));
-
-//task 2
-
-const doMath = (x, znak, y) =>{
-    if(isNaN(x) || isNaN(y)) return "some element is not a number";
-    if(znak === '+'){
-        return x + y;
-    }
-    else if(znak === '-'){
-        return x - y;
-    }
-    else if(znak === '*'){
-        return x * y;
-    }
-    else if(znak === '/'){
-        return y !== 0 ? x/y : "you can't divide by zero.";
-    }
-    else if (znak === '^'){
-        return x ** y;
-    }
-    else if(znak === '%'){
-        return y !== 0 ? x%y : "you can't divide by zero.";
-    }
-    else{
-        return "error with operator";
-    }
-}
-const firstNumber = +prompt('write first number');
-const operator = prompt('write operator');
-const secondNumber = +prompt('write second number');
-console.log(doMath(firstNumber,operator,secondNumber));
-
-//task 3
-
-const userData = (numberOfExternalArray, numberOfInnerArray) =>{
-
-    if(isNaN(numberOfExternalArray) || isNaN(numberOfInnerArray)) return "some element is not a number";
+const Shift = (array) => {
+    if(array.length === 0) return array;
 
     let result = [];
+    const firstVariable = array[0];
 
-    let data;
-
-    for (let i = 0; i < numberOfExternalArray; i++) {
-
-        data = prompt(`insert data using only ${numberOfInnerArray} elements, dividing by ","`);
-
-        let innerArray = data.split(',');
-
-        if(innerArray.length > numberOfInnerArray){
-            innerArray = innerArray.slice(0, numberOfInnerArray);
-        }
-
-        while(innerArray.length < numberOfInnerArray) {
-            innerArray.push('*empty*')
-        }
-
-        result.push(innerArray);
+    for(let i = 1; i < array.length; i++){
+        result[result.length] = array[i];
     }
 
-    return result;
-}
-const numberOfExternalArray = +prompt('number of external array');
-const numberOfInnerArray = +prompt('number of inner array');
-console.log(userData(numberOfExternalArray,numberOfInnerArray));
-
-//task 4
-
-const removeSymbols = (text, array) => {
-    let result = '';
-    for (let i = 0; i < text.length; i++) {
-        if (!array.includes(text[i])) {
-            result += text[i];
-        }
+    for(let i = 0; i < result.length; i++){
+        array[i] = result[i];
     }
-    return result;
+    array.length = result.length;
+
+    return firstVariable;
 }
 
-const inputText = prompt('write any text');
-const inputArrayOfSymbols = prompt('write array of symbols dividing ","');
-const arrayOfSymbols = inputArrayOfSymbols.split(',');
-console.log(removeSymbols(inputText, arrayOfSymbols));
+let array = ['1', 2, '3', 4, '5'];
+let variable = Shift(array);
+
+console.log(variable);
+console.table(array);
+
+const Reverse = (array) => {
+    if(array.length === 0) return array;
+
+    let reverseArray = [];
+
+    for(let i = array.length - 1; i >= 0; i--){
+        reverseArray[reverseArray.length ] = array[i];
+    }
+
+    for(let i = 0; i < reverseArray.length; i++){
+
+        array[i] = reverseArray[i];
+    }
+
+    return array;
+}
+
+Reverse(array);
+
+console.table(array);
