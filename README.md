cba87a7cf539012a6ddc91fa623fd00c165cc021
fgr-fgg958156 <fgg958156@gmail.com>
2025-06-26 12:18:55 +0300

    Completed hw-29

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# script about "Hoisting and Scope"
+# script about "User accounts"
 
-## Write a function that demonstrates the difference between declaring variables using var, let, and const inside and outside a loop.
-
-Task:
-* Create a for loop, inside which you declare a variable using var, and another using let.
-* After the loop finishes, try to print both variables to the console.
-* Also add another const variable, and try to print it both inside and outside the loop.
-* Explain what happens to the hoisting and scope of each variable.
+Given an array of objects. Output an array of phone numbers of users whose balance is more than 2000 dollars. And find the sum of all user balances
--- js/script.js
+++ js/script.js
@@ -1,58 +1,82 @@
 "use strict";
 
-const _function = () =>{
-    //Created a 'for' loop with 1 iteration
-    for(let i = 0; i < 1; i++){
-        var _varValue = i;// value var
-        let _letValue = i * 2;// value let
-        const _constValue = i * 3;// value const
-
-        //announced in loop
-        console.log("*loop*");
-        console.log(`var in loop ${_varValue}`);
-        console.log(`let in loop ${_letValue}`);
-        console.log(`const in loop ${_constValue}`);
+let users = [
+    {
+        "index": 0,
+        "isActive": true,
+        "balance": "$2,226.60",
+        "name": "Eugenia Sawyer",
+        "gender": "female",
+        "phone": "+1 (840) 583-3207",
+        "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+        "index": 1,
+        "isActive": true,
+        "balance": "$2,613.77",
+        "name": "Pauline Gallegos",
+        "gender": "female",
+        "phone": "+1 (985) 593-3328",
+        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+        "index": 2,
+        "isActive": false,
+        "balance": "$3,976.41",
+        "name": "Middleton Chaney",
+        "gender": "male",
+        "phone": "+1 (995) 591-2478",
+        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+        "index": 3,
+        "isActive": true,
+        "balance": "$1,934.58",
+        "name": "Burns Poole",
+        "gender": "male",
+        "phone": "+1 (885) 559-3422",
+        "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+        "index": 4,
+        "isActive": true,
+        "balance": "$3,261.65",
+        "name": "Mcfadden Horne",
+        "gender": "male",
+        "phone": "+1 (942) 565-3988",
+        "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+        "index": 5,
+        "isActive": false,
+        "balance": "$1,790.56",
+        "name": "Suzette Lewis",
+        "gender": "female",
+        "phone": "+1 (837) 586-3283",
+        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
     }
+]
 
-
-    //announced without loop
-    console.log("*without loop*");
-    console.log(_varValue);
-
-    try{
-        console.log(_letValue);
+const SearchPhoneNum = (Array) =>{
+    let result = [];
+    for(let i = 0; i < Array.length; i++){
+        let balance = +(Array[i].balance.replace(/[$,]/g, ""));
+        if(balance > 2000){
+            result.push(Array[i].phone);
+        }
     }
-    catch(err){
-        console.log("_letValue:", err);
-    }
-
-    try{
-        console.log(_constValue);
-    }
-    catch(err){
-        console.log("_constValue:", err);
-    }
+    return result;
 }
-_function();
 
-/*
-created function "_function"
-called a function
+const SumOfBalance = (Array) =>{
+    let result = 0;
+    for(let i = 0; i < Array.length; i++){
+        let balance = +(Array[i].balance.replace(/[$,]/g, ""));
+        result += balance;
+    }
+    return result;
+}
 
-created a loop
-added to loop 3 value (var, let, const)
-announced 3 value in loop
-ended loop
+console.table(SearchPhoneNum(users));
+console.log(SumOfBalance(users));
 
-announced 3 value without loop
-
-var give value because it is a "function scope" or "Функціональна область визначення"
-in hosting "var" is raised to the beginning of the function and "var" works anywhere in the function
-
-let give error "is not defined" because it is a "block scope" or "Блочна область визначення"
-in hosting "let" is in Temporal Dead Zone and "let" works only in the loop
-
-const give error "is not defined" because it is a "block scope" or "Блочна область визначення"
-in hosting "const" is in Temporal Dead Zone and "const" works only in the loop
-
-*/
