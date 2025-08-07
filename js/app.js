18f0a3deb2f310ab54b685c01b244b35cb3194ca
fgr-fgg958156 <fgg958156@gmail.com>
2025-08-07 11:55:59 +0300

    completed hw-46

--- js/app.js
+++ js/app.js
@@ -1,42 +1,8 @@
 'use strict';
-import Navigation from "./Navigation.js";
+import URLParser from "./URLParser.js";
 
-const mainPageLink = document.getElementById('home');
-const aboutPageLink = document.getElementById('about');
-const contactPageLink = document.getElementById('contact');
-const pageTitle = document.getElementById('title');
-
-const arrayOfPages = [mainPageLink, aboutPageLink, contactPageLink]
-const newNavigation = new Navigation(arrayOfPages);
-
-// mainPageLink.addEventListener('click', e => {
-//     e.preventDefault();
-//     history.pushState({page: 'home'}, `title of page home`, 'home');
-// })
-eventClick(mainPageLink, 'home', 'home', 'home');
-eventClick(aboutPageLink, 'about', 'about', 'about');
-eventClick(contactPageLink, 'contact', 'contact', 'contact');
-
-function UpdateTitle(){
-    pageTitle.innerHTML = window.location.pathname.replace("/hwgit/",'');
-}
-
-window.addEventListener('popstate', function(e){
-    if(e.state){
-        console.log("current state:", e.state);
-        newNavigation.CheckURL();
-        UpdateTitle();
-    }
-    else{
-        console.warn('this page has no state');
-    }
-});
-
-function eventClick(pageLink, pageName, pageTitle, pageUrl){
-    pageLink.addEventListener('click', e => {
-        e.preventDefault();
-        history.pushState({page: pageName}, `title of page ${pageTitle}`, pageUrl);
-        newNavigation.CheckURL();
-        UpdateTitle();
-    })
-}
+const parser = new URLParser("https://example.com/products/item?search=book&page=2");
+console.log(parser.queryParams); // { search: "book", page: "2" }
+console.log(parser.protocol);
+console.log(parser.hostname);
+console.log(parser.path);
